20b668ab9248a55b99de28cbd75240bb0ffdae0c
Shaun.xiongzheng <shaun_zheng@163.com>
2022-01-01 21:04:52 -0500

    1

---
+++ sidebar.js
@@ -0,0 +1,60 @@
+<div id="sidebar">
+    <div class="inner">
+
+            <section id="search" class="alt">
+                <form method="post" action="#">
+                    <input type="text" name="query" id="query" placeholder="Search" />
+                </form>
+            </section>
+
+
+            <nav id="menu">
+                <header class="major">
+                    <h2>Catalog</h2>
+                </header>
+                <ul>
+                    <li><a href="index.html">Homepage</a></li>
+                    <li><a href="resume.html">CV</a></li>
+                    <li>
+                        <span class="opener">Machine Learning</span>
+                        <ul>
+                            <li><a href="Projects/Assignment6_NJTrainDelays.html">Train Delay Prediction</a></li>
+                            <li><a href="Projects/Assignment4_BusinessAnalyze.html">Cost Benefit Optimization</a></li>
+                            <li><a href="Projects/Assignment2_housePricePrediction.html">House Price Prediction</a></li>
+                            <li><a href="Projects/Assignment3_ChicagoNarcotics.html">Narcotic Abuse Prediction</a></li>
+                            <li><a href="Projects/Assignment5_CitiBike.html">Ride Bike Share Prediction</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <span class="opener">Data Visualization</span>
+                        <ul>
+                            <li><a href="#">Lorem Dolor</a></li>
+                            <li><a href="#">Ipsum Adipiscing</a></li>
+                            <li><a href="#">Tempus Magna</a></li>
+                            <li><a href="#">Feugiat Veroeros</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Maximus Erat</a></li>
+                    <li><a href="#">Sapien Mauris</a></li>
+                    <li><a href="#">Amet Lacinia</a></li>
+                </ul>
+            </nav>
+
+            <section>
+                <header class="major">
+                    <h2>Get in touch</h2>
+                </header>
+                <ul class="contact">
+                    <li class="icon solid fa-envelope"><a href="mailto:zhxiong@upenn.edu">zhxiong@upenn.edu</a></li>
+                    <li class="icon solid fa-phone">(267) 324-1399</li>
+                    <li class="icon brands fa-linkedin"><a href="http://www.linkedin.com/in/shaun-zheng">Xiong Zheng</a></li>
+                    <li class="icon brands fa-github"><a href="https://github.com/ShaunZhxiong">ShaunZhxiong</a></li>
+                </ul>
+            </section>
+
+            <footer id="footer">
+                <p class="copyright">&copy; Shaun's Portfolio. All rights reserved.</p>
+            </footer>
+
+    </div>
+</div>
